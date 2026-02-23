fef402cf4a0b0984e1c123835092a9ed0e582235
kaizju <rookieslit@gmail.com>
2026-02-23 15:10:29 +0800

    added user_activity_logs table

--- db/working_schema.sql
+++ db/working_schema.sql
@@ -6,3 +6,12 @@ create table users (
     updated_at timestamp default current_timestamp
     on update current_timestamp
 ) ENGINE=InnoDB
+
+create table user_activity_logs (
+    activity_id int auto_increment primary key,
+user_id int,
+action varchar(50) not null,
+created_at timestamp default current_timestamp,
+foreign key (user_id) references user(user_id)
+
+) ENGINE=InnoDB
