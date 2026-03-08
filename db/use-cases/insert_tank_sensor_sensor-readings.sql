dcab5a16aeb961a92aaada63e543e530fd6e7649
kaizju <rookieslit@gmail.com>
2026-03-08 17:10:48 +0800

    added changes

--- db/use-cases/insert_tank_sensor_sensor-readings.sql
+++ db/use-cases/insert_tank_sensor_sensor-readings.sql
@@ -6,8 +6,8 @@ VALUES ('jude');
 INSERT INTO user_activity_logs (user_id, action)
 VALUES (LAST_INSERT_ID(), 'put status of the tank');
 
-INSERT INTO tank (tankname, location_add, capacity, status_tank)
-VALUES ('Tank 1', 'Jude Crib', '3,542L', 'Active');
+INSERT INTO tank (tankname, location_add, capacity, max_capacity, status_tank)
+VALUES ('Tank 1', 'Jude Crib', 1200, 5000, 'Active');
 
 INSERT INTO sensors (tank_id, sensor_type, model, unit, is_active)
 VALUES (LAST_INSERT_ID(), 'Water Level', 'Model 1', '1', 'Active');
